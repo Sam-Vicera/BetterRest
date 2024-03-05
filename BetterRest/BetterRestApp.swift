251ed3bbb0375d57fe82ed0f2cf942cddf105531
Samuel Hernandez Vicera <shernandezvicera@apple.com>
2024-03-05 14:02:16 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Samuel Hernandez Vicera on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
